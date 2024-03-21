4eb04cd05b0a2ec84d980367eff49d18779d95bf
Misterguppy <emetpark@gmail.com>
2024-03-21 14:36:59 -0400

    Create Numbersum.java

---
+++ Numbersum.java
@@ -0,0 +1,34 @@
+/**Emet Park
+ * 3/21/24
+ * numbersum
+ */
+
+import java.util.*;
+public class Numbersum {
+    public static void main(String[] args){
+        int count = 1;
+        int tot = 0;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Give a number:");
+        int num = input.nextInt();
+        input.close();
+        boolean counted = false;
+        System.out.println('1');
+        while(!counted){
+            
+        
+           
+            count += 1;
+            tot+= count;
+            System.out.println(count);
+            if(count == num){
+                counted = true;
+                System.out.println(tot);
+            }
+
+
+        
+       
+       }
+    }
+}
