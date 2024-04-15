9458faafa56275884e2bce0fb4dde3170f423537
Misterguppy <emetpark@gmail.com>
2024-04-15 14:24:22 -0400

    Update DigitSum.java

--- DigitSum.java
+++ DigitSum.java
@@ -4,18 +4,19 @@ import java.util.*;
 public class DigitSum {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter a positive integer: ");
+        System.out.print("Enter a positive integer: ");
         int pos = input.nextInt();
         input.close();
         int mods = 10;
         int tot = 0;
-        if(pos > mods)
+        while(pos > mods){
             pos = pos % mods;
             tot = tot + pos;
             mods = mods* 10;
-        
+        }
+           
 
-        System.out.println("The sum of the digits is: "+tot);
+        System.out.print("The sum of the digits is: "+tot);
     }
 
 }
